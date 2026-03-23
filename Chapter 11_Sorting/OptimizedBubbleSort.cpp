18ae4a36f08cf926a2132aaa44d73fde172d0687
Your Name <mohitagrawal2860@gmail.com>
2026-03-23 20:37:31 +0530

    Optimized Version of Bubble Sort which Gives Time Complexity of O(n) in Best Case and O(n^2) in Worst Case

---
+++ Chapter 11_Sorting/OptimizedBubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n){
+    for(int i=1; i<n; i++){
+        bool swapped = false;
+        for(int j=0; j<n-i; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(swapped = false){
+            break;
+        }
+    }
+}
+int main(){
+    int arr[] = {10, 1, 9, 7, 14, 80};
+    int n = 6;
+    cout << "Array Sorted using Bubble Sort: "; 
+    bubbleSort(arr, n);
+    for(int i=0 ; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
